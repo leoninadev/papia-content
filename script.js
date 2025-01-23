9afcb57852483d7e20590df9242f5175bf8e5bf6
leoninadev <149819978+leoninadev@users.noreply.github.com>
2025-01-23 14:59:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// Adicionando efeito de paralaxe ao Banner 3
+window.addEventListener('scroll', function() {
+    const banner3 = document.querySelector('.banner-3');
+    const scrollPosition = window.pageYOffset;
+
+    // Ajuste a velocidade do paralaxe alterando o valor de '0.4'
+    banner3.style.backgroundPositionY = scrollPosition * 0.4 + 'px';
+});
